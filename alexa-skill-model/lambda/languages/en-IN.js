855d77f45cf8ef9afc52d2bac46220e860504748
Ansh <anshsaxena4190@gmail.com>
2023-07-23 11:46:21 +0530

    added skill model

---
+++ alexa-skill-model/lambda/languages/en-IN.js
@@ -0,0 +1,75 @@
+// en.js
+module.exports = {
+  translation: {
+    SKILL_NAME: 'Mint My words',
+    GREETING: [
+      'Hello. Welcome to %s. Here you can mint a NFT of your words on Flow blockchain. Would you like to try?',
+      'Hi. Welcome to %s. Would you like to mint a NFT of your words on Flow blockchain?',
+    ],
+    GREETING_REPROMPT: [
+      'Would you like to mint your words as a NFT?',
+    ],
+    SCHEDULE_YES: [
+      'Okay, let\'s mint your words.',
+      'Okay, let\'s get started.',
+    ],
+    SCHEDULE_NO: [
+      'All right. Stop back whenever you\'d like to mint a NFT.',
+      'Okay, I\'ll be here whenever you want to smint a NFT.'
+    ],
+    HELP: [
+      'This skill can help you Mint a NFT of your words as a Image on Flow blockchain, and mail it to you. Would you like to try this awesome project?',
+    ],
+    HELP_REPROMPT: [
+      'Would you like to mint a NFT?',
+    ],
+    CANCEL_STOP_RESPONSE: [
+      'Good bye',
+      'Okay. I\'ll be here if you need me.',
+    ],
+    FALLBACK: [
+      'You can say: you would like to schedule an appointment or that you have an appointment request to make. Would you like to schedule an appointment?',
+    ],
+    FALLBACK_REPROMPT: [
+      'Would you like to schedule an appointment?',
+    ],
+    APPOINTMENT_CONFIRM: [
+      'Going to mint: %s. Would you like to confirm this request?',
+    ],
+    APPOINTMENT_CONFIRM_REPROMPT: [
+      'Should I mint for %s?',
+    ],
+    APPOINTMENT_CONFIRM_COMPLETED: [
+      'Your appointment with %s on %s has been scheduled. You\'ll also recieve an email confirmation. Please reply to the email confirmation to cancel or reschedule. Thank you.',
+    ],
+    TIME_AVAILABLE: [
+      '%s is available. Would you like to book it?',
+    ],
+    TIME_AVAILABLE_REPROMPT: [
+      'Would you like to book %s ?',
+    ],
+    TIME_NOT_AVAILABLE: [
+      'Sorry, %s is not available. Would you like to try another time?',
+    ],
+    TIME_NOT_AVAILABLE_REPROMPT: [
+      'Would you like to try another time?',
+    ],
+    APPOINTMENT_TITLE: 'Appointment with %s',
+    APPOINTMENT_DESCRIPTION: 'This is a telephone appointment for %s with %s. We will call you at %s. If you need to cancel or change this appointment, please reply to this email.',
+    EMAIL_SUBJECT: 'Appointment for %s with %s',
+    EMAIL_TEXT: 'This is a telephone appointment for %s with %s. We will call you at %s. If you need to cancel or change this appointment, please reply to this email.',
+    NO_CONFIRM: 'Okay, to start over you can say: schedule an appointment, or to cancel say stop.',
+    NO_CONFIRM_REPROMOT: 'You can say schedule an appointment to start over, or stop to cancel.',
+    ENV_NOT_CONFIGURED: 'One or more environment variables is not set. Please see the readme file for help.',
+    PERMISSIONS_REQUIRED: 'In order to book and confirm appointments, %s will need access to your name, email address, and phone number. To provide access, please enable profile permissions in the Amazon Alexa app.',
+    EMAIL_REQUIRED: 'It looks like you don\'t have an email set. You can set your email in the Alexa companion app.',
+    EMAIL_REQUIRED_REPROMPT: 'Please set your email address in the Alexa companion app.',
+    NAME_REQUIRED: 'It looks like you don\'t have your name set. You can set your name in the Alexa companion app.',
+    NAME_REQUIRED_REPROMPT: 'Please set your name in the Alexa companion app.',
+    PHONE_REQUIRED: 'It looks like you don\'t have your mobile number set. You can set your mobile number in the companion app.',
+    PHONE_REQUIRED_REPROMPT: 'Please set your phone number in the Alexa companion app.',
+    ERROR: 'Sorry, I didn\'t get that. Could you say that again?',
+    ERROR_REPROMPT: 'Could you say that again?',
+    FREEBUSY_DISABLED: 'Sorry, freebusy checking is disabled. Would you like to schedule an appointment anyway?',
+  },
+};
